11e4cb2d3cf3bd80504eb14dde37b1816eaba039
Skyeler McQueen <scm237@cornell.edu>
2023-12-05 10:57:23 -0500

    Added in caching to main function

--- spotofy_api.py
+++ spotofy_api.py
@@ -60,7 +60,7 @@ def parse_playlist(api_output):
             artists[name]={'id':id,
                         'api_link':api_link}
     return artists
-print("---------------------------------------------------------------------------------")
+
 
 
 def get_artist_genres(link,headers):
@@ -114,41 +114,50 @@ def parse_wikimedia_request(name, wiki_result, artists_full_info):
 
     temp_json = wiki_result.json()
     english_info=None
-
-    for num in range(len(temp_json)):
-        if 'in_language' in temp_json[num].keys():
-            if temp_json[num]['in_language']['identifier']=='en':
-                english_info = temp_json[num]['infobox']
     birth = None
     died = None
     occupation = []
     instruments = []
+    print(name)
+    try:
+        for num in range(len(temp_json)):
+            if 'in_language' in temp_json[num].keys():
+                if temp_json[num]['in_language']['identifier']=='en':
+                    english_info = temp_json[num]['infobox']
 
-    for item in english_info[0]['has_parts'][0]['has_parts']:
-        try:
-            if item['name']=='Born':
-                birth = item['value']
-        except:
-            print("--")
-        try:
-            if item['name']=='Died':
-                died = item['value']
-        except:
-            pass
-        try:
-            if 'occupation' in item['name'].lower():
-                occupation = item['value'].lower().split(" ")
-        except:
-            pass
-        try:
-            if 'instrument' in item['name'].lower():
-                instruments = item['value'].lower().split(" ")
-        except:
-            pass
-    artists_full_info[name]={'birth':birth,
-                             'died': died,
-                             'instruments': instruments,
-                             'occupations': occupation}
+        for item in english_info[0]['has_parts'][0]['has_parts']:
+            try:
+                if item['name']=='Born':
+                    birth = item['value']
+            except:
+                continue
+            try:
+                if item['name']=='Died':
+                    died = item['value']
+            except:
+                continue
+            try:
+                if 'occupation' in item['name'].lower():
+                    occupation = item['value'].lower().split(" ")
+            except:
+                continue
+            try:
+                if 'instrument' in item['name'].lower():
+                    instruments = item['value'].lower().split(" ")
+            except:
+                continue
+            if died ==None:
+                died = 'alive'
+    except:
+        print('----------Nah')
+    artists_full_info['birth']=birth
+    artists_full_info['died']=died
+    artists_full_info['instruments']=instruments
+    artists_full_info['occupations']=occupation
+    #artists_full_info[name]={'birth':birth,
+                                # 'died': died,
+                                # 'instruments': instruments,
+                                # 'occupations': occupation}
 # parse_wikimedia_request('Ella Fitzgerald', wiki_test, artists_full_info)
 # parse_wikimedia_request('Selena Gomez', wiki_two,artists_full_info)
 
@@ -156,7 +165,7 @@ def parse_wikimedia_request(name, wiki_result, artists_full_info):
 #born = info_box_info['Born']
 
 def main():
-    token ="BQCGMGhKtzRJc8kRxPDhx7S1NLJtraGqbMYvsEQGea0qpRLt-g8JcFm1CKGpketn6NOZMwzCcvSDmOoQHFr3SFSKccIOgU2iSYY6GqGy9wjPe8DzVg0"
+    token ="BQAggjr-lFD6x9nF__7f5IUKttlALCV_Pfz5VoXBWmQg9i6V4wcCdp98hS_15Wz_UwlmRMvrQiInwunnH5-IiW3bNkn_RK7RDqWqoCclXKdzLnV4YdE"
 
     headers={"Authorization": f"Bearer {token}"}
 
@@ -171,19 +180,25 @@ def main():
     test_link = "https://api.spotify.com/v1/artists/07YZf4WDAMNwqr4jfgOZ8y"
     get_artist_genres(test_link,headers)
 
-    wikimedia_token ="eyJraWQiOiJzeVNnS1JaZWdwcDFlSGZEYnlsR2YrTnBjVmVXUDZJNGJlSFpOWjBDZVdrPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiIxNGQ2MGMxOC04MGQ2LTQ5NzAtYTA4Mi0yYTY4MTRkMzFjZDkiLCJjb2duaXRvOmdyb3VwcyI6WyJncm91cF8xIl0sImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC51cy1lYXN0LTEuYW1hem9uYXdzLmNvbVwvdXMtZWFzdC0xX0tiNW5ZZDN6dSIsImNsaWVudF9pZCI6IjY0MXU0aTdncHR1ZmZzc2w0bTlvYXR2NHU5Iiwib3JpZ2luX2p0aSI6ImQyOTI2MDU3LWI3MjYtNGZkMC05MWMwLTk1NjgyN2U4YzEyYSIsImV2ZW50X2lkIjoiMDMyM2JjMzAtZDg1YS00NjE3LThhMTktMTdhZjRmZDhmYTAxIiwidG9rZW5fdXNlIjoiYWNjZXNzIiwic2NvcGUiOiJhd3MuY29nbml0by5zaWduaW4udXNlci5hZG1pbiIsImF1dGhfdGltZSI6MTcwMTQ2NDcyNiwiZXhwIjoxNzAxNTUxMTI2LCJpYXQiOjE3MDE0NjQ3MjYsImp0aSI6ImM5OThkZWI3LTdmZTMtNGY1OC1hMjg1LWMyZWVmMDMzNDUyNCIsInVzZXJuYW1lIjoic2t5ZWxlcmJlYXIifQ.dStqBiuARmLYz_k6tEpbOnjenjQevBsZXo_EcQdgubDLTSkmHIzVZNP_EzHGT4uj6AkkVYiXFdZjTt5fHWiZTVB0ufGR3JndBwb3D5n62j8blwZoE5FNI8gFz9Aio0c4TUqwoEoDXjPRiXIVeGH0LimNkH3nrS_qQsLya_zJ3e8E2A6-3VVoSummjVZhDw99a5swqzQR-SIGig3e1RU4sKB8v7ThUMDAiT5wlXUgia5LI-Cx6HWaQ5zxZI0VF0kiIY_iNwK9QGeqybKcPkez0e7qvsDMkh4SDsEb_ZkoO1kRKbhAjGoWwodfix45hi-GYnBSr0lfEesvz5wKx-X6EA"
+    wikimedia_token ="eyJraWQiOiJzeVNnS1JaZWdwcDFlSGZEYnlsR2YrTnBjVmVXUDZJNGJlSFpOWjBDZVdrPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiIxNGQ2MGMxOC04MGQ2LTQ5NzAtYTA4Mi0yYTY4MTRkMzFjZDkiLCJjb2duaXRvOmdyb3VwcyI6WyJncm91cF8xIl0sImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC51cy1lYXN0LTEuYW1hem9uYXdzLmNvbVwvdXMtZWFzdC0xX0tiNW5ZZDN6dSIsImNsaWVudF9pZCI6IjY0MXU0aTdncHR1ZmZzc2w0bTlvYXR2NHU5Iiwib3JpZ2luX2p0aSI6ImFlM2RlZTU5LWRlZjEtNGViOS05Njk0LWJjZWU1YTQ2YWIwZiIsImV2ZW50X2lkIjoiZTJmMjdmYWUtNWVmZi00NWM0LTlmNDQtOTRhNjA1NjEyN2E1IiwidG9rZW5fdXNlIjoiYWNjZXNzIiwic2NvcGUiOiJhd3MuY29nbml0by5zaWduaW4udXNlci5hZG1pbiIsImF1dGhfdGltZSI6MTcwMTc4ODQ0MSwiZXhwIjoxNzAxODc0ODQxLCJpYXQiOjE3MDE3ODg0NDEsImp0aSI6ImU1ZTRiMTk1LTVkYjEtNDZmMy04OWFkLTdiMzA5YjEyOTgwMyIsInVzZXJuYW1lIjoic2t5ZWxlcmJlYXIifQ.SOwOkuenPGk6o8bCQHDbWBPdvfnSNC-MBzwGFdpsiWFaxSqJQifQuosDkRy5JcCOEW2hpeTESOIlVQKFvALEdVb1dm5hiCXZfA1as2C6pKNeadHn_5QkaoyjZ7Y3BiC444dnlLWg9eE6b_daK-P6pMg8_EVltd2L4up8kQK1Ag3wS00SJkwfyiuEr6-_5RGu6P4z5UsRo0Fq_c09MF8bC5Mac1HYufjoo0wQPXJZRyASCrzY3qPq6Zg-GFgKHSOe79QqR9uOSK_JIMugAlJNY7g62lIbidy-3mMkxqKTX2c2oYTgoxuFovkdMlyhIyhf4um-9wagrNNzBkuJW2zd_w"
     wiki_headers = {"Authorization": f'Bearer {wikimedia_token}'}
 
     for celeb in parsed_100.keys():
         if celeb not in spotify_cache.keys():
             more_deets = get_artist_genres(parsed_100[celeb]['api_link'],headers)
+
+            wiki_deets = wikimedia_request(wikimedia_token,celeb)
+            parse_wikimedia_request(celeb,wiki_deets,more_deets)
+            print(more_deets)
+            
             for item in more_deets.keys():
                 parsed_100[celeb][item]=more_deets[item]
-            spotify_cache[celeb]=parsed_100[celeb][item]
-        
-    print(parsed_100['Rema'])
-
-
+            
+            spotify_cache[celeb]=parsed_100[celeb]
+        else: #already cached
+            parsed_100[celeb] = spotify_cache[celeb]
+    save_cache(spotify_cache,CACHE_FILENAME)
+    print(parsed_100['Katy Perry'].keys())
 
 if __name__ == '__main__':
     main()
