eb336a16782b7968d09ecdf424fad2fbc8b349bb
Skyeler McQueen <scm237@cornell.edu>
2023-12-07 12:32:11 -0500

    troubleshooting to find panel hates lists

--- panel_display.py
+++ panel_display.py
@@ -9,8 +9,16 @@ import nx_altair as nxa
 import traceback
 import pygraphviz
 import graph_networkx as gnx
-from panel.theme import Material #might mess with this...
 import network_functions as ntf
+import json
+import requests
+import csv 
+import requests
+import wikipediaapi
+import classes as cs
+import json
+import networkx as nx
+from networkx.readwrite import json_graph
 
 pn.extension('vega')
 alt.renderers.enable('default')
@@ -44,32 +52,49 @@ sidecol.append(start_button)
 #define function to bind
 def click_start(event):
     global graph
-    graph = ntf.parse_playlist(playlist_input.value)
-    return None
-#bind them!
-start_button.on_click(click_start)
+    #graph = ntf.parse_playlist(playlist_input.value)
 
+# playlist_graph = pn.panel(alt.Chart(pd.DataFrame([], columns=['data'])).mark_bar().encode(
+#             x='data:Q',
+#         ).properties(title="Waiting for data..."))
+
+data =  {'directed': False, 'graph': {}, 'links': [{'source': 'Lia Pappas-Kemps', 'target': 'pov: indie'},{'source': 'Lia Pappas-Kemps', 'target': 'toronto indie'}], 'multigraph': False, 'nodes': [{'id':'Lia Pappas-Kemps','type':'artist'},{'id':'toronto indie','type':'genre'},{'id':'pov: indie','type':'genre'},{'id':'bussy','type':'genre'},{'id':'baddy','type':'genre'},{'id':'Lia 11Pappas-Kemps','type':'artist'},{'id':'toronto in111111die','type':'genre'},{'id':'pov: 11111indie','type':'genre'},{'id':'bus111111sy','type':'genre'},{'id':'bad1111dy','type':'genre'}]}
+
+print("ABOUT TO LOAD")
+#graph= ntf.load_example_graph()
+
+# ex_file = open('test_graph_data',"r")
+# graph_contents = ex_file.read()
+# graph_dict = json.loads(graph_contents)
+# ex_file.close()
+graph = nx.node_link_graph(data)
+
+#poppy = ntf.filter_popularity(graph,'50')
+test2 ={'directed': False, 'multigraph': False, 'graph': {}, 'nodes': [{'birth': 'Maisie Hannah Peters 28 May 2000 Steyning, England', 'died': 'alive', 'type': 'artist', 'id': 'Maisie Peters'}, {'type': 'genre', 'id': 'alt z'}, {'type': 'genre', 'id': 'uk pop'},  {'birth': None, 'died': None,  'type': 'artist', 'id': 'Chappell Roan'}, {'type': 'genre', 'id': 'indie pop'}, {'type': 'genre', 'id': 'springfield mo indie'}], 'links': [{'source': 'Maisie Peters', 'target': 'alt z'}, {'source': 'Maisie Peters', 'target': 'uk pop'}, {'source': 'Chappell Roan', 'target': 'indie pop'}, {'source': 'Chappell Roan', 'target': 'springfield mo indie'}]}
+poppy=nx.node_link_graph(test2)
+pop=ntf.draw_network(poppy,labels=True,size_v=400).interactive()
+
+graph = nx.node_link_graph(data)
+playlist_graph = pn.panel(ntf.draw_network(poppy,labels=True,size_v=400).interactive())
+print("OBJECT UPDATED")
+##GODDAMN GET RID OF THOSE MFIN LISTS
+
+row1=pn.Row()
+title_panel = pn.pane.Markdown('''# Playlist
+                               ''')
+
+row1.append(playlist_graph)
+print("ADDED MY GRAPH TO ROW 1")
 template = pn.template.BootstrapTemplate(
-    title='Analyze your playlist',
+    title='507 Dashboard',
     sidebar=sidecol
 )
 
-#set up title 
-title_panel = pn.pane.Markdown(f''' # Let's analyze your playlist [PLACEHOLDER]
-                               ''')
-#set up row 1
-row1 = pn.Row()
-
-#big graph
-playlist_graph = pn.panel(blank_chart)
-
-#genre chart
-
-
-
-
-row1.append(playlist_graph)
-
+print("can we get here or no")
 
 template.main.append(title_panel)
+print("hmm")
+template.main.append(row1)
+print("just added row 1")
 template.servable()
+print("ITS NOT ")
