ad6bea3e89e7e29cfb5adb2b78996de770580bd7
Skyeler McQueen <scm237@cornell.edu>
2023-12-10 14:18:53 -0500

    made load_example_graph_file.py stand alone

--- load_example_graph_file.py
+++ load_example_graph_file.py
@@ -2,6 +2,37 @@ import network_functions as ntf
 import networkx as nx 
 import json 
 
+def load_example_graph(graph_name='test_graph_data',dict_name='test_lookup_data',name="Currently by skyelermcquill"):
+    '''
+    Loads a networkx graph from a json file with a node-link graph data. 
+
+    Returns a networkx graph! This function was made to save a step from loading API data. 
+
+    PARAMETERS
+    ----------
+    graph_name: str
+        the name of the json file you want to load with graph data
+    dict_name: str
+        the name of the json file you want to load with lookup dict data
+    name: str
+        name of the playlist you're loading
+
+    RETURNS
+    -------
+    list: [networkx graph, dict]
+        a node link graph objects
+    '''
+    ex_file = open(graph_name,"r")
+    graph_contents = ex_file.read()
+    graph_dict = json.loads(graph_contents)
+    ex_file.close()
+
+    dict_file = open(dict_name,"r")
+    lookup_cont = dict_file.read()
+    lookup = json.loads(lookup_cont)
+    dict_file.close()
+    return [nx.node_link_graph(graph_dict),lookup,name]
+
 example = ntf.load_example_graph()
 print(f'Here is the example graph: {example[0]}')
 print(f'Here are the example nodes: {example[0].nodes}')
